37262967394366fea0566e28688e9b7654e0963a
mimeault <martin.mimeault@nrcan-rncan.gc.ca>
2023-02-09 13:30:22 -0500

    update config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,27 +1,35 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# -- Project information
 
 project = 'CDTK Documentation'
-copyright = '2023, Martin Mimeault, Natural resources Canada'
-author = 'Martin Mimeault, Natural resources Canada'
+copyright = '2023, Natural resources Canada'
+author = 'Martin Mimeault'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+release = '0.1'
+version = '0.1.0'
 
-extensions = []
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-exclude_patterns = []
 
+# -- Options for HTML output
 
+html_theme = 'sphinx_rtd_theme'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
